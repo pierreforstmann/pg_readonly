e15aac300c0d07ba6f7a75a4ec055986d89696f5
Pierre Forstmann <15964998+pierreforstmann@users.noreply.github.com>
2021-08-31 16:36:13 +0200

    Create test.sql

---
+++ sql/test.sql
@@ -0,0 +1,40 @@
+--
+select get_cluster_readonly();
+select unset_cluster_readonly();
+--
+drop table t;
+drop function f;
+--
+create table t(i int);
+
+create function f(IN p int, OUT i int) returns int as $$
+begin
+    insert into t(i) values (f.p) returning t.i into f.i;
+end$$
+language plpgsql;
+
+select set_cluster_readonly();
+
+select * from t;
+
+insert into t values (1);
+
+select f(1);
+-- However, in this same session, if you call f() before entering in read only mode, 
+-- it is then able to write during read only mode:
+
+select unset_cluster_readonly();
+
+select f(1);
+
+select set_cluster_readonly();
+
+insert into t values (2);
+
+select f(2);
+
+select get_cluster_readonly();
+
+select f(3);
+
+select * from t;
